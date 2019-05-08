8fc9c16e7628035410608d7a54f75ca7e9645bc7
Brooke Bryan <brooke@bajb.net>
2019-05-08 09:25:11 +0100

    Quick start

--- src/PathBuilder.php
+++ src/PathBuilder.php
@@ -1,6 +1,69 @@
 <?php
 namespace JDI\Helper;
 
+use function implode;
+
 class PathBuilder
 {
+  /**
+   * Concatenate a path with a custom separator
+   *
+   * @param string   $separator
+   * @param string[] $pathComponents
+   *
+   * @return string
+   */
+  public static function custom($separator, array $pathComponents)
+  {
+    return implode($separator, $pathComponents);
+  }
+
+  /**
+   * Concatenate any number of path sections and correctly
+   * handle directory separators
+   *
+   * @param array $parts
+   *
+   * @return string
+   */
+  public static function system(...$parts)
+  {
+    return static::custom(DIRECTORY_SEPARATOR, $parts);
+  }
+
+  /**
+   * Concatenate a path with windows style path separators
+   *
+   * @param array $parts
+   *
+   * @return string
+   */
+  public static function windows(...$parts)
+  {
+    return static::custom('\\', $parts);
+  }
+
+  /**
+   * Concatenate a path with unix style path separators
+   *
+   * @param array $parts
+   *
+   * @return string
+   */
+  public static function unix(...$parts)
+  {
+    return static::custom('/', $parts);
+  }
+
+  /**
+   * Concatenate a path with unix style path separators
+   *
+   * @param array $parts
+   *
+   * @return string
+   */
+  public static function url(...$parts)
+  {
+    return static::custom('/', $parts);
+  }
 }
